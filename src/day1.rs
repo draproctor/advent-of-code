aae9301b496c48d2bc6da88d1ab459c5ebb91d11
Austin Proctor <ajhproctor@gmail.com>
2023-12-24 13:14:51 -0800

    Add doc comments, improve function names

--- src/day1.rs
+++ src/day1.rs
@@ -34,31 +34,51 @@ pub fn solve(file_path: PathBuf) {
     println!("The total is {}", total);
 }
 
+/// Given a line in a file, find the first and last numbers in the line. The digits are combined
+/// to form a two digit number.
+///
+/// # Examples
+/// ```
+/// let line1 = String::from("trom7bone");
+/// let line2 = String::from("asdf4jkdsqseven");
+/// let line3 = String::from("asdasfourdsqwefiveasdastwobar");
+/// assert_eq!(parse_line(line1), 77);
+/// assert_eq!(parse_line(line2), 47);
+/// assert_eq!(parse_line(line3), 42);
+/// ```
 fn parse_line(line: String) -> i32 {
     let numbers = extract_numbers(&line);
     let first_number = numbers.first().unwrap();
     let second_number = numbers.last().unwrap();
-    return format!("{}{}", first_number, second_number)
+    format!("{}{}", first_number, second_number)
         .parse::<i32>()
-        .unwrap();
+        .unwrap()
 }
 
 /// Given a string, extract all number literals and written out numbers.
 fn extract_numbers(line: &str) -> Vec<String> {
     line.char_indices()
-        .filter_map(|(index, c)| extract_number_from_index(index, &c, &line))
+        .filter_map(|(index, c)| find_number_string(index, c, &line))
         .collect()
 }
 
-fn extract_number_from_index(index: usize, c: &char, line: &str) -> Option<String> {
+/// Given an index in a string, the character at that index, and a string, determine if that
+/// character is either a number or the beginning of a word of a number.
+fn find_number_string(index: usize, c: char, line: &str) -> Option<String> {
     match c {
         '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => Some(c.to_string()),
-        'o' | 't' | 'f' | 's' | 'e' | 'n' => extract_number_from_word(line, index),
+        'o' | 't' | 'f' | 's' | 'e' | 'n' => get_word_of_number(line, index),
         _ => None,
     }
 }
 
-fn extract_number_from_word(line: &str, index: usize) -> Option<String> {
+/// Given a line and an index in that line, search for words that describe numbers,
+/// such as `"one"`, `"eight"`, or `"nine"`.
+///
+/// These numbers are at most five characters long, so this function takes the index and looks five
+/// characters further. If this string slice begins with any number-word, then the corresponding
+/// number is returned as string.
+fn get_word_of_number(line: &str, index: usize) -> Option<String> {
     let last_index = line.len().min(index + 5);
     match line.get(index..last_index) {
         Some(slice) => NUMBERS.iter().find_map(|(number, value)| {
